54660ecda50ea4906ac631fe32d2d557520f3746
deepshikhalcb049 <lcb2021049@iiitl.ac.in>
2023-07-28 03:30:12 +0530

    ekdum latest

--- src/cards.js
+++ src/cards.js
@@ -7,11 +7,11 @@ function cards() {
     return (
         <div className=' bg-black relative flex shrink'>
             <div className='w-[100%] items-center justify-center relative flex flex-col'>
-                <div className='mt-[300px] w-[90%] items-center justify-center  flex relative'>
-                    <p className='gt bg-clip-text text-white w-[905px]'>Experience your product as you create it.</p>
+                <div className='min-[380px]:mt-[300px] mt-[100px] w-[90%] items-center justify-center  flex relative'>
+                    <p className='gt bg-clip-text text-white min-[380px]:w-[905px] w-[380px]  '>Experience your product as you create it.</p>
                 </div>
-                <div className='mt-[100px] h-[1200px] w-[85%] mr-[-50px] relative  flex flex-col shrink'>
-                    <div className=' h-[48%] relative grid grid-cols-3  flex-col shrink'>
+                <div className='min-[380px]:mt-[100px] mt-[30px] min-[380px]:h-[1200px] h-[340px] w-[95%] min-[380px]:mr-[-50px] relative  flex flex-col shrink'>
+                    <div className=' h-[48%] min-[380px]:pl-[15px] min-[380px]:pr-[15px] relative grid grid-cols-3 flex-col shrink'>
                         <div className='  col-span-2'>
                             <Atropos
                                 className="atropos-banner"
@@ -27,15 +27,15 @@ function cards() {
                             // shadow={true}
                             // shadowOffset={2}
                             >
-                                <div className='w-[785px] h-[520px] border-[0.08px] border-gray-700 rounded-[26px] bg-[#161B22] relative'>
+                                <div className='min-[380px]:w-[785px] min-[380px]:h-[520px] h-[150px] ml-[10px] w-[200px] border-[0.08px] border-gray-700 min-[380px]:rounded-[26px] rounded-[10px] bg-[#161B22]  relative'>
                                     <img src={require('./img/a.png')} alt="" className='' />
-                                    <p className="t1 text-transparent  bg-clip-text bg-gradient-to-r from-[#F5F5F8] to-[#BBBBBD]  mt-[30px]">Effortlessly tracking with Black</p>
-                                    <p className='t2  w-[487px] inline-block'>Designed for you, Black is the perfect solution for you and
+                                    <p className="t1 text-transparent  bg-clip-text bg-gradient-to-r from-[#F5F5F8] to-[#BBBBBD]  mt-[18px] min-[380px]:mt-[30px]">Effortlessly tracking with Black</p>
+                                    <p className='t2  min-[380px]:w-[487px] w-[150px] h-[100px] inline-block'>Designed for you, Black is the perfect solution for you and
                                         your teams tracking time."</p>
                                 </div>
                             </Atropos>
                         </div>
-                        <div className='ml-[-40px] relative flex flex-col shrink'>
+                        <div className='min-[380px]:ml-[-100px] ml-[-10px] relative flex flex-col shrink'>
                             <Atropos
                                 className="atropos-banner"
                                 highlight={false}
@@ -50,7 +50,7 @@ function cards() {
                             // shadow={true}
                             // shadowOffset={2}
                             >
-                                <div className='w-[445px] h-[520px] relative  border-[0.08px] border-gray-700 rounded-[26px] bg-[#161B22]'>
+                                <div className='min-[380px]:w-[445px] min-[380px]:h-[520px] h-[150px] min-[380px]:left-[50px] w-[110px] left-[px] relative  border-[0.08px] border-gray-700 min-[380px]:rounded-[26px] rounded-[10px] bg-[#161B22]'>
                                     <img src={require('./img/b.png')} alt="" />
                                     <p className="t1 text-transparent  bg-clip-text bg-gradient-to-r from-[#F5F5F8] to-[#BBBBBD]">Save your time</p>
                                     <p className='t2'>Available on iOS and Android, Black is accessible
@@ -60,7 +60,7 @@ function cards() {
                         </div>
                     </div>
                     <div className=' h-[48%]   relative grid grid-cols-3'>
-                        <div className='mr-[-20px]'>
+                        <div className='min-[380px]:mr-[-20px] relative left-[10px]'>
                             <Atropos
                                 className="atropos-banner"
                                 highlight={false}
@@ -75,15 +75,15 @@ function cards() {
                             // shadow={true}
                             // shadowOffset={2}
                             >
-                                <div className='w-[445px] h-[520px]  border-[0.08px] border-gray-700 rounded-[26px] bg-[#161B22]'>
+                                <div className='min-[380px]:w-[445px] min-[380px]:h-[520px] h-[150px] min-[380px]:left-[50px] w-[110px] relative  border-[0.08px] border-gray-700 min-[380px]:rounded-[26px] rounded-[10px] bg-[#161B22]'>
                                     <img src={require('./img/d.png')} alt="" />
                                     <p className="t1 text-transparent  bg-clip-text bg-gradient-to-r from-[#F5F5F8] to-[#BBBBBD] z-20">Tracking</p>
-                                    <p className='t2 w-[403px] inline-block z-20'>Comes with built-in workflows that create focus
+                                    <p className='t2 min-[380px]:w-[403px] w-[90px] inline-block z-20'>Comes with built-in workflows that create focus
                                         and routine.</p>
                                 </div>
                             </Atropos>
                         </div>
-                        <div className='relative ml-[50px] col-span-2 flex flex-col shrink'>
+                        <div className='relative min-[380px]:ml-[50px] ml-[13px] col-span-2 flex flex-col shrink'>
                             <Atropos
                                 className="atropos-banner"
                                 highlight={false}
@@ -98,7 +98,7 @@ function cards() {
                             // shadow={true}
                             // shadowOffset={2}
                             >
-                                <div className='w-[785px] h-[520px]  border-[0.08px] border-gray-700 rounded-[26px] bg-[#161B22]'>
+                                <div className='min-[380px]:w-[785px] min-[380px]:h-[520px] h-[150px]  w-[200px] border-[0.08px] border-gray-700 min-[380px]:rounded-[26px] rounded-[10px] bg-[#161B22]  relative'>
                                     <p className="t1 text-transparent  bg-clip-text bg-gradient-to-r from-[#F5F5F8] to-[#BBBBBD] mt-[20px] z-20">Experience seamless tracking</p>
                                     <p className='t2'>Awarded App of the day, Black has been recognized as the
                                         top choice for teams and individuals."</p>
--- src/nav.js
+++ src/nav.js
@@ -26,38 +26,38 @@ function nav() {
     return (
         <div className=' bg-black flex flex-col shrink'> 
             <div className='sticky flex  shrink justify-center  z-50'>
-                <div className="sm:w-[700px] min-[370px]:w-[500px] w-[300px]  h-[55px] bg-[#212121]  rounded-[100px] fixed top-[10px]">
+                <div className="sm:w-[700px] min-[380px]:w-[500px] w-[300px]  h-[55px] bg-[#212121]  rounded-[100px] fixed top-[10px]">
                     <div class="flex  shrink justify-between ">
                         <div className=' left-[19px] top-[16px] relative '>
                             <div className='sm:w-[80px] w-[50px] h-[44px] '> <img src={require('./img/loo.png')} alt="" /></div>
                         </div>
-                        <div className='flex min-[370px]:text-[14px] text-[8px] relative min-[370px]:w-[200px] w-[150px] gap-x-[0px] min-[370px]:gap-x-[5px] sm:w-[300px] justify-between text-[#81859F] top-[15px]' style={text1}>
+                        <div className='flex min-[380px]:text-[14px] text-[8px] relative min-[380px]:w-[200px] w-[150px] gap-x-[0px] min-[380px]:gap-x-[5px] sm:w-[300px] justify-between text-[#81859F] top-[15px]' style={text1}>
                             <div className=''>Feautures</div>
                             <div className=''>About</div>
                             <div className=''>Pricing</div>
                             <div className=''>Blog </div>
                         </div>
 
-                        <div className='relative top-[10px] right-[21px]'> <button className='bg-white rounded-[100px] w-[43px] min-[370px]:w-[83px] min-[370px]:h-[35px] h-[20px]' style={text1}>Sign up</button> </div>
+                        <div className='relative top-[10px] right-[21px]'> <button className='bg-white rounded-[100px] w-[43px] min-[380px]:w-[83px] min-[380px]:h-[35px] h-[20px]' style={text1}>Sign up</button> </div>
                     </div>
                 </div>
             </div>
-            <div className='flex flex-col shrink  w-min-screen mt-[0px] min-[370px]:h-[1050px] h-[850px] bg-black z-[0]     justify-center items-center     ' id='neww'>
-                <div className='w-auto  min-[370px]:top-[250px] top-[300px] flex  sticky'>
-                    <p className='relative text-transparent  text-[25px] min-[370px]:text-[93px] bg-clip-text bg-gradient-to-r from-[#EEC5BD] via-[#F3B1A5]  via-[#C6CAF6] to-[#DAD9E9]' style={st}>Take charge of your money & Grow it, effortlessly.</p>
+            <div className='flex flex-col shrink  w-min-screen mt-[0px] min-[380px]:h-[1050px] h-[800px] bg-black z-[0]     justify-center items-center     ' id='neww'>
+                <div className='w-auto  min-[380px]:top-[250px] top-[300px] flex  sticky'>
+                    <p className='relative text-transparent  text-[25px] min-[380px]:text-[93px] bg-clip-text bg-gradient-to-r from-[#EEC5BD] via-[#F3B1A5]  via-[#C6CAF6] to-[#DAD9E9]' style={st}>Take charge of your money & Grow it, effortlessly.</p>
                 </div>
-                <div className='flex flex-col justify-center w-min-screen min-[370px]:top-[-280px] top-[-100px]  relative  items-center '>
-                    <img src={require('./img/phone.png')} alt="" srcset="" className='min-[370px]:w-[294px] w-[120px] min-[370px]:h-[600px] h-[280px]' />
+                <div className='flex flex-col justify-center w-min-screen min-[380px]:top-[-280px] top-[-100px]  relative  items-center '>
+                    <img src={require('./img/phone.png')} alt="" srcset="" className='min-[380px]:w-[294px] w-[120px] min-[380px]:h-[600px] h-[280px]' />
 
                 </div>
             </div>
             <div className='h-[350px] w-min-screen bg-black flex flex-col shrink items-center '>
-                <div className=' '><p className='texx  min-[370px]:leading-[26.6px] leading-[15px] min-[370px]:text-[18px] text-[7px] min-[370px]:w-[716px] w-[300px]'>Effortlessly track and manage your time with Black. Customizable reports, invoicing, and integrations make it the perfect tool for freelancers and professionals.</p>
+                <div className=' '><p className='texx min-[380px]:text-[18px] text-[7px] min-[380px]:w-[716px] w-[300px]'>Effortlessly track and manage your time with Black. Customizable reports, invoicing, and integrations make it the perfect tool for freelancers and professionals.</p>
                 </div>
 
 
                 <div className='flex relative mt-[45px] ml-[150px] items-center '>
-                    <input type="text " className='border-0 h-[50px] w-[460px] area text-white' placeholder="Enter your email" id="clear" />
+                    <input type="text " className='border-0 min-[380px]:h-[50px] min-[380px]:w-[460px] h-[40px] w-[350px] area text-white' placeholder="Enter your email" id="clear" />
                     <button id="butt" className='areabut'>Start Free Trial</button>
                 </div>
             </div>
