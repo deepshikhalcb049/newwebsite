3cc651f591f1ae4c44bc0a5d7ac83bcf2c97be17
deepshikhalcb049 <lcb2021049@iiitl.ac.in>
2023-07-28 01:57:31 +0530

    new files

--- src/nav.js
+++ src/nav.js
@@ -2,7 +2,7 @@ import react from 'react';
 import Logo from './img/logo.png'
 const text1 = {
     fontFamily: " Inter",
-    fontSize: "14px",
+ 
     fontStyle: "normal",
     fontWeight: "500",
     lineHeight: "16.8px",
@@ -10,7 +10,7 @@ const text1 = {
 const st = {
     textAlign: "center",
     // fontFamily: "Manrope",
-    fontSize: "93.8px",
+    // fontSize: "93.8px",
     fontStyle: "normal",
     fontWeight: "700",
     lineHeight: "normal",
@@ -26,33 +26,33 @@ function nav() {
     return (
         <div className=' bg-black flex flex-col shrink'> 
             <div className='sticky flex  shrink justify-center  z-50'>
-                <div className="w-[700px] h-[55px] bg-[#212121]  rounded-[100px] fixed top-[10px]">
+                <div className="sm:w-[700px] min-[370px]:w-[500px] w-[300px]  h-[55px] bg-[#212121]  rounded-[100px] fixed top-[10px]">
                     <div class="flex  shrink justify-between ">
                         <div className=' left-[19px] top-[16px] relative '>
-                            <div className='w-[80px] h-[44px] '> <img src={require('./img/loo.png')} alt="" /></div>
+                            <div className='sm:w-[80px] w-[50px] h-[44px] '> <img src={require('./img/loo.png')} alt="" /></div>
                         </div>
-                        <div className='flex relative w-[300px] justify-between text-[#81859F] top-[15px]' style={text1}>
+                        <div className='flex min-[370px]:text-[14px] text-[8px] relative min-[370px]:w-[200px] w-[150px] gap-x-[0px] min-[370px]:gap-x-[5px] sm:w-[300px] justify-between text-[#81859F] top-[15px]' style={text1}>
                             <div className=''>Feautures</div>
                             <div className=''>About</div>
                             <div className=''>Pricing</div>
                             <div className=''>Blog </div>
                         </div>
 
-                        <div className='relative top-[10px] right-[21px]'> <button className='bg-white rounded-[100px] w-[83px] h-[35px]' style={text1}>Sign up</button> </div>
+                        <div className='relative top-[10px] right-[21px]'> <button className='bg-white rounded-[100px] w-[43px] min-[370px]:w-[83px] min-[370px]:h-[35px] h-[20px]' style={text1}>Sign up</button> </div>
                     </div>
                 </div>
             </div>
-            <div className='flex flex-col shrink  w-min-screen mt-[0px] h-[1050px] bg-black z-[0]     justify-center items-center     ' id='neww'>
-                <div className='w-auto  top-[250px] flex  sticky'>
-                    <p className='relative text-transparent  text-8xl bg-clip-text bg-gradient-to-r from-[#EEC5BD] via-[#F3B1A5]  via-[#C6CAF6] to-[#DAD9E9]' style={st}>Take charge of your money & Grow it, effortlessly.</p>
+            <div className='flex flex-col shrink  w-min-screen mt-[0px] min-[370px]:h-[1050px] h-[850px] bg-black z-[0]     justify-center items-center     ' id='neww'>
+                <div className='w-auto  min-[370px]:top-[250px] top-[300px] flex  sticky'>
+                    <p className='relative text-transparent  text-[25px] min-[370px]:text-[93px] bg-clip-text bg-gradient-to-r from-[#EEC5BD] via-[#F3B1A5]  via-[#C6CAF6] to-[#DAD9E9]' style={st}>Take charge of your money & Grow it, effortlessly.</p>
                 </div>
-                <div className='flex flex-col justify-center w-min-screen top-[-280px]  relative  items-center '>
-                    <img src={require('./img/phone.png')} alt="" srcset="" className='w-[294px] h-[600px]' />
+                <div className='flex flex-col justify-center w-min-screen min-[370px]:top-[-280px] top-[-100px]  relative  items-center '>
+                    <img src={require('./img/phone.png')} alt="" srcset="" className='min-[370px]:w-[294px] w-[120px] min-[370px]:h-[600px] h-[280px]' />
 
                 </div>
             </div>
             <div className='h-[350px] w-min-screen bg-black flex flex-col shrink items-center '>
-                <div className=' '><p className='texx w-[716px]'>Effortlessly track and manage your time with Black. Customizable reports, invoicing, and integrations make it the perfect tool for freelancers and professionals.</p>
+                <div className=' '><p className='texx  min-[370px]:leading-[26.6px] leading-[15px] min-[370px]:text-[18px] text-[7px] min-[370px]:w-[716px] w-[300px]'>Effortlessly track and manage your time with Black. Customizable reports, invoicing, and integrations make it the perfect tool for freelancers and professionals.</p>
                 </div>
 
 
